07a0ad3ce866b281b04970cf7c82c0baf71514a2
Quang Minh Nguyen <quangng711@outlook.com.au>
2025-03-10 22:44:05 +1100

    Initial Commit

---
+++ NrvMap 3D/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  NrvMap 3D
+//
+//  Created by Quang Minh Nguyen on 3/10/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ NrvMap 3D/NrvMap_3DApp.swift
@@ -0,0 +1,33 @@
+//
+//  NrvMap_3DApp.swift
+//  NrvMap 3D
+//
+//  Created by Quang Minh Nguyen on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NrvMap_3DApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.progressive), in: .progressive)
+    }
+}
