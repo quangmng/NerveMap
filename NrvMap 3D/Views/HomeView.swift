e0154abdc37a910d1452ab71cee641eebab164e7
Quang Minh Nguyen <quangng711@outlook.com.au>
2025-04-03 17:38:59 +1100

    btn improvements, removing need for extension

--- NrvMap 3D/Views/HomeView.swift
+++ NrvMap 3D/Views/HomeView.swift
@@ -11,60 +11,89 @@ import RealityKit
 import RealityKitContent
 
 struct HomeView: View {
-  
+    
     var body: some View {
         NavigationStack{
-            VStack{
-                
-                NavigationLink(destination: ChooseModelView()) {
-                    ZStack {
-                        Rectangle()
-                            .foregroundColor(.clear)
-                            .frame(width: 1220, height: 220)
-                            .background(Color.gray.opacity(0.7))
-                            .cornerRadius(25)
-                        
+            GeometryReader { geometry in
+                VStack(spacing: 10) {
+                    NavigationLink(destination: ChooseModelView()) {
                         Text("Choose Model")
                             .font(.extraLargeTitle)
                             .foregroundColor(.white)
-                            .leading()
-                            .padding()
+                            .frame(maxWidth: .infinity, minHeight: 300, alignment: .center)
                             
                     }
+                    .buttonBorderShape(.roundedRectangle(radius: 25))
+                    .padding(.horizontal)
+//                    .frame(maxWidth: .infinity)
+                    
+                    
+                    HStack(alignment: .center, spacing: 10) {
+                        NavigationLink{
+                            SpaceControl()
+                        } label: {
+                            Text("🏃\nSimulation")
+                                .font(.extraLargeTitle)
+                                .foregroundStyle(Color.white)
+                                .frame(maxWidth: 300, minHeight: 100, alignment: .center)
+                                .padding(.horizontal, 30)
+                        }
+                        
+                        
+                        
+                        NavigationLink{
+                            ChooseModelView()
+                        } label: {
+                            Text("📝\nSaved Note")
+                                .font(.extraLargeTitle)
+                                .foregroundStyle(Color.white)
+                                .frame(maxWidth: 300, minHeight: 100, alignment: .center)
+                                .padding(.horizontal, 30)
+                        }
+                        
+                        NavigationLink{
+                            SearchView()
+                        } label: {
+                            Text("🔍\nSearch")
+                                .font(.extraLargeTitle)
+                                .foregroundStyle(Color.white)
+                                .frame(maxWidth: 300, minHeight: 100, alignment: .center)
+                                .padding(.horizontal, 30)
+                        }
+                    }
+                    .buttonBorderShape(.roundedRectangle(radius: 25))
+                    .padding(.horizontal)
+                    
+                    Spacer().frame(height: 20)
+                    
+                    NavigationLink {
+                        HelpView()
+                    } label: {
+                        Text("❓Help")
+                            .font(.extraLargeTitle)
+                            .foregroundStyle(Color.white)
+                            .padding()
+                            .frame(width: 300)
+                    }
+                    .buttonBorderShape(.roundedRectangle(radius: 25))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding([.leading, .bottom])
+
+//                    CustomNavLink(title: "❓Help", destination: HelpView())
+//                        .frame(width: geometry.size.width * 0.3, height: geometry.size.height * 0.1)
                 }
-                .buttonStyle(.plain)
-                .padding()
-                
-                
-                HStack{
-                    
-                    CustomNavLink(title: "🏃Simulation", destination: SpaceControl())
-                    
-                    
-                    CustomNavLink(title: "📝Saved Note", destination: ChooseModelView())
-                    
-                    CustomNavLink(title: "🔍Search", destination: SearchView())
-                    
-                    
-                }
-                
-                CustomNavLink(title: "❓Help", destination: HelpView())
-                    .frame(width: 250, height: 100)
-                    .leading()
-                
-                
-                
-            }
-                    
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Text("NrvMap3D")
-                        .font(.extraLargeTitle)
-                        .fontDesign(.rounded)
+                .fontDesign(.rounded)
+                .frame(maxWidth: .infinity, minHeight: 150, alignment: .center)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Text("NrvMap3D")
+                            .font(.extraLargeTitle)
+                            .fontDesign(.rounded)
+                            .padding(.horizontal, 50)
+                    }
                 }
             }
         }
-        .padding()
     }
 }
 
