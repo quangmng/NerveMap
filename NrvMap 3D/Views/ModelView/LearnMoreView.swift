8108b3e9347c065e22ab9ab90d5b0071a256fefe
Nocolororororo <150432737+Nocolororororo@users.noreply.github.com>
2025-05-20 23:22:30 +1000

    Wende's learn more view update

--- NrvMap 3D/Views/ModelView/LearnMoreView.swift
+++ NrvMap 3D/Views/ModelView/LearnMoreView.swift
@@ -12,24 +12,25 @@ struct LearnMoreView: View {
     @State private var selectedNerveLevel: String? = nil
     @State private var selectedArea: String? = nil
     @EnvironmentObject var fvm: FunctionViewModel
-
+    
     // Compute unique first-letter categories (e.g., V, C, T, L, S)
     private var nerveLevelPrefixes: [String] {
         let prefixes = allDermatomes.map { String($0.nerveLevel.prefix(1)) }
         return Array(Set(prefixes)).sorted()
     }
-
+    
     // Helper method to filter dermatomes by nerve level
     private func dermatomesForNerveLevel(_ level: String) -> [Dermatome] {
         return allDermatomes.filter { $0.nerveLevel.hasPrefix(level) }
     }
-
+    
     var body: some View {
         VStack {
-            Spacer()
-
+            
             // Nerve Level Selection
-            VStack {
+            VStack(alignment: .leading) {
+                Text("Choose the dermatome area👇🏻")
+                    .font(.system(size: 55, design: .rounded))
                 HStack {
                     ForEach(nerveLevelPrefixes, id: \.self) { prefix in
                         Button {
@@ -61,58 +62,62 @@ struct LearnMoreView: View {
                     }
                 }
                 .padding(.bottom, 20)
-
+                
                 // Subcategory List (e.g., T1, T2, etc.)
                 if let nerveLevel = selectedNerveLevel {
-                    ScrollView(.horizontal) { 
-                        HStack{
-                            ForEach(dermatomesForNerveLevel(nerveLevel)) { dermatome in
-                                Button {
-                                    withAnimation {
-                                        selectedArea = dermatome.nerveLevel
-                                        
-                                        if fvm.selectedEntity != nil {
-                                            fvm.stopBlinkingHighlight(for: fvm.selectedEntity!)
-                                        }
-                                        
-                                        if fvm.isMale == false {
-                                            fvm.selectedEntity = fvm.femaleModel?.findEntity(named: dermatome.nerveLevel.lowercased())
-                                            fvm.startBlinkingHighlight(for: fvm.selectedEntity!)
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        VStack(alignment: .leading){
+                            Text("Choose More Details👇🏻")
+                                .font(.system(size: 55, design: .rounded))
+                            HStack{
+                                ForEach(dermatomesForNerveLevel(nerveLevel)) { dermatome in
+                                    Button {
+                                        withAnimation {
+                                            selectedArea = dermatome.nerveLevel
                                             
+                                            //                                        if fvm.selectedEntity != nil {
+                                            //                                            fvm.stopBlinkingHighlight(for: fvm.selectedEntity!)
+                                            //                                        }
+                                            //
+                                            //                                        if fvm.isMale == false {
+                                            //                                            fvm.selectedEntity = fvm.femaleModel?.findEntity(named: dermatome.nerveLevel.lowercased())
+                                            //                                            fvm.startBlinkingHighlight(for: fvm.selectedEntity!)
+                                            //
+                                            //                                        }
+                                            //                                        else if fvm.isMale == true{
+                                            //                                            fvm.selectedEntity = fvm.maleModel?.findEntity(named: dermatome.nerveLevel.lowercased())
+                                            //                                            fvm.startBlinkingHighlight(for: fvm.selectedEntity!)
+                                            //                                        }
+                                            //                                        print(fvm.selectedEntity?.name)
                                         }
-                                        else if fvm.isMale == true{
-                                            fvm.selectedEntity = fvm.maleModel?.findEntity(named: dermatome.nerveLevel.lowercased())
-                                            fvm.startBlinkingHighlight(for: fvm.selectedEntity!)
-                                        }
-                                        print(fvm.selectedEntity?.name)
-                                    }
-                                } label: {
-                                    Text(dermatome.nerveLevel)
-                                        .font(
-                                            .system(
-                                                size: 60,
-                                                weight: .bold,
-                                                design: .rounded
+                                    } label: {
+                                        Text(dermatome.nerveLevel)
+                                            .font(
+                                                .system(
+                                                    size: 60,
+                                                    weight: .bold,
+                                                    design: .rounded
+                                                )
                                             )
-                                        )
-                                        .foregroundColor(.primary)
-                                        .frame(width: 120, height: 100)
-                                        .padding()
+                                            .foregroundColor(.primary)
+                                            .frame(width: 120, height: 100)
+                                            .padding()
+                                    }
+                                    .buttonBorderShape(.circle)
+                                    .padding()
                                 }
-                                .buttonBorderShape(.circle)
-                                .padding()
                             }
                         }
                     }
                     .transition(.opacity)
                 }
             }
-
+            
             // Display selected dermatome info (e.g., "T1" info)
             if let area = selectedArea {
                 if let dermatome = allDermatomes.first(where: { $0.nerveLevel == area }) {
                     VStack(alignment: .leading) {
-                        Text(dermatome.area)
+                        Text("Area Detail: \(dermatome.area)")
                             .font(.system(size: 50, weight: .semibold))
                             .foregroundColor(.primary)
                             .padding(.bottom, 5)
@@ -122,8 +127,8 @@ struct LearnMoreView: View {
                                     .fill(Color.gray)
                                     .frame(maxWidth: .infinity)
                             }
-
-                        Text(dermatome.clinicalNote)
+                        
+                        Text("More Info: \(dermatome.clinicalNote)")
                             .font(.system(size: 45, weight: .regular))
                             .foregroundColor(.primary)
                             .padding()
@@ -132,10 +137,7 @@ struct LearnMoreView: View {
                                     .fill(Color.gray.opacity(0.5))
                                     .frame(maxWidth: .infinity)
                             }
-//                            .padding()
-
                     }
-                    .padding()
                     .transition(.opacity)
                 }
             }
@@ -146,7 +148,8 @@ struct LearnMoreView: View {
     }
 }
 
-//#Preview {
-//    LearnMoreView()
-//}
+#Preview(windowStyle: .automatic) {
+    LearnMoreView()
+        .environmentObject(FunctionViewModel())
+}
 
